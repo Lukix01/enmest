43881359432f8c5c0130185e1383c9e1d60e2993
Lukix01 <e@lklos.com>
2022-04-02 12:21:19 +0200

    menu

--- src/ui/menu.lua
+++ src/ui/menu.lua
@@ -3,12 +3,12 @@ require("src/ui/button")
 Menu = {}
 
 function Menu:load()
-    self.width = 200
-    self.height = 400
+    self.width = 350
+    self.height = 450
     Button:load()
 end
 
 function Menu:draw()
-    love.graphics.rectangle("fill", love.graphics.getWidth() / 2 - (self.width / 2), love.graphics.getHeight() / 2 - (self.height / 2), self.width, self.height)
-    Button:draw()
+    Button:draw("Start")
+    Button:draw("Exit")
 end
