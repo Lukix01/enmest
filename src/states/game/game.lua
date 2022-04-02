c4645f63d87e40d67eb0e1a96f983ed9b2a13ab9
Lukix01 <e@lklos.com>
2022-04-02 23:10:51 +0200

    investments object

--- src/states/game/game.lua
+++ src/states/game/game.lua
@@ -1,31 +1,35 @@
 require("src/utils/timer")
-require("src/states/game/ui/optionButton")
+require("src/states/game/ui/button")
 require("src/states/game/ui/box")
 
-Game = {}
+Game = {
+}
+
+Stats = {
+    investments = {
+        houses = 1,
+        lcoins = 0,
+        hotels = 0
+    },
+    money = 100,
+    income = 20
+}
+
 Box = {
     margin = 10,
     buttons = {
         { text = "Buy a new house", fn = nil },
-        { text = "Pay bills (0$)", fn = nil },
+        { text = "Pay bills", fn = nil },
         { text = "Go to work", fn = nil }
     }
 }
 
-Stats = {
-    investments = {},
-    money = 100,
-    income = 20
-}
 
 function Game:load()
     Box.width = 600
     Box.height = 250
     Box.x = love.graphics.getWidth() / 2 - Box.width / 2
     Box.y = love.graphics.getHeight() / 2 - Box.height / 2
-    Stats.investments.houses = 1
-    Stats.investments.lcoins = 0
-    Stats.investments.hotels = 0
     Stats.income = 20
     GameBox:load()
 end
