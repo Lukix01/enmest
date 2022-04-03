e63ae9fc29286faa5c3e226106f1400f967f1424
Lukix01 <e@lklos.com>
2022-04-03 16:25:10 +0200

    fixed game box

--- src/states/game/ui/box.lua
+++ src/states/game/ui/box.lua
@@ -1,13 +1,11 @@
-require("src/events/hover")
 GameBox = {}
 Button = {}
-
 
 function GameBox:load()
     Button.font = love.graphics.newFont(15)
     Button.width = Box.width / 3
     Button.height = 50
-    Button.y = 400
+    Button.y = 395
 end
 
 function GameBox:draw()
@@ -19,19 +17,8 @@ function GameBox:draw()
     love.graphics.printf("Income: " .. Stats.income .. "$/day", love.graphics.newFont(18), 0, 320, 1280, "center")
     love.graphics.setColor(0.5, 0.5, 0.5, 1)
     love.graphics.printf("Houses: " .. Stats.investments[1].amount .. " | " .. "Lcoins: " .. Stats.investments[2].amount .. " | " .. "Hotels: " .. Stats.investments[3].amount, love.graphics.newFont(18), 0, 350, 1280, "center")
-    love.graphics.setColor(1, 1, 1, 1)
     for i, button in ipairs(Box.buttons) do
-        button.pressed = button.down
-        Button.mx, Button.my = love.mouse.getPosition()
-        Button.hover = Hover(Button.mx, Button.my, button.x, Button.y, Button.width, Button.height)
-        button.down = love.mouse.isDown(1)
         love.graphics.setColor(0.3, 0.5, 0.2, 1)
-        if Button.hover then
-            love.graphics.setColor(0.3, 0.5, 0.2, 0.7)
-            if button.down and not button.pressed then
-                button.fn()
-            end
-        end
         love.graphics.rectangle("fill", button.x, Button.y, Button.width, Button.height)
         love.graphics.setColor(1, 1, 1, 1)                                                                          
         love.graphics.print(button.text, Button.font, button.x + Button.width / 2 - Button.font:getWidth(button.text) / 2, Button.y + Button.height / 2 - Button.font:getHeight() / 2)
