8cdaa92574b923d88282a27474155802986b39c6
Lukix01 <e@lklos.com>
2022-04-03 17:51:09 +0200

    upgrade key module

--- src/modules/key.lua
+++ src/modules/key.lua
@@ -1,9 +1,24 @@
-Key = {}
+Key = {
+    instances = {}
+}
+
+function checkKey(checkedKey)
+    for i, key in ipairs(Key.instances) do
+        if checkedKey == key.key then
+            return true
+        end
+    end
+end
 
 function Key:down(userKey, fn)
+    if not checkKey(userKey) then
+        table.insert(Key.instances, { key = userKey, fn = fn})
+    end
     function love.keypressed(key)
-        if key == userKey then
-            fn()
+        for i, instanceKey in ipairs(Key.instances) do
+            if key == instanceKey.key then
+                instanceKey.fn()
+            end
         end
     end 
 end
